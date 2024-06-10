dc77a0598f22380454e7169caa9bd6b6aba37c14
Jacques <jacques.poulin64@gmail.com>
2024-06-10 07:32:43 +0200

    LandinPage part 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import Image from 'next/image';
+import Hero from '@/components/Hero';
+import { FloatingNav } from '@/components/ui/FloatingNav';
+import Grid from '@/components/Grid';
+import { FaHome } from 'react-icons/fa';
+
 
 export default function Home() {
 	return (
 		<main className='relative flex justify-center items-center flex-col overflow-hidden mx-auto bg-black-100  px-5 sm:px-10'>
 			<div className='max-w-7xl w-full'>
-				<h1>HELLO</h1>
+				<FloatingNav
+					navItems={[{ name: 'Home', link: '/', icon: <FaHome/> }]}
+				/>
+				<Hero />
+				<Grid/>
 			</div>
 		</main>
 	);
